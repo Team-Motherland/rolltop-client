6d3878744b5610b67cf5540e1b10b4be3c0336f3
Alina Kitaeva <akitaeva@gmail.com>
2018-08-10 08:49:03 -0400

    more styling

--- src/app/components/desktop/desktop.component.ts
+++ src/app/components/desktop/desktop.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NotesService } from '../../services/notes.service';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '../../../../node_modules/@angular/router';
+import { PlacesService } from '../../services/places.service';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-desktop',
@@ -11,13 +13,17 @@ import { Router } from '../../../../node_modules/@angular/router';
 export class DesktopComponent implements OnInit {
 
   allTheNotes: Array<Object> = [];
+  allTheProjects: Array<Object> = [];
+  allThePlaces: Array<Object> = [];
   listError: String = '';
   logoutError: String = '';
   theUser: any = {};
 
-  constructor(private myNotesService: NotesService,
-    private myAuthService: AuthService,
-    private myRouter: Router) { }
+  constructor( private myAuthService: AuthService,
+    private myRouter: Router,
+    private myPlacesService: PlacesService,
+    private myProjectService: TodoService,
+    private myNotesService: NotesService) { }
 
   ngOnInit() {
     this.myAuthService.checklogin()
@@ -27,6 +33,8 @@ export class DesktopComponent implements OnInit {
       this.theUser = resFromDB;
     } )
     this.showNoteList();
+    this.showPlaceList();
+    this.showProjects();
   }
 
   showNoteList(){
@@ -36,5 +44,24 @@ export class DesktopComponent implements OnInit {
     },
     () => this.listError = 'Sorry! No notes! Something went bad on the backend route!')
   }
+
+  showPlaceList(){
+
+    this.myPlacesService.getAllPlaces()
+    .subscribe( allPlaces => {
+      this.allThePlaces = allPlaces;
+
+    },
+    () => this.listError = 'Sorry! No fav places! Something went bad on the backend route!')
+  }
+
+  showProjects(){
+    this.myProjectService.getProjects()
+    .subscribe( allProjects => {
+      this.allTheProjects = allProjects;
+    },
+    () => this.listError = 'Sorry! No notes! Something went bad on the backend route!')
+  }
+
 
 }
